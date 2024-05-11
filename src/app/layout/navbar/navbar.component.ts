e4d2a4f78a17af1a13f28b760050442ace72d730
Sahil-Ahmed19 <ahmedsahilcr7@gmail.com>
2024-05-11 20:29:06 +0530

    installed moduled

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { MaterialModule } from '../../material/material.module';
 
 @Component({
