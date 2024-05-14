dc4adc370b9a2aca40765ba7ce7248d582bd041e
Sahil-Ahmed19 <ahmedsahilcr7@gmail.com>
2024-05-15 00:30:44 +0530

    login signup configuraqtion completed

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'amader-chuti-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+
+  router = inject(Router);
+  fb = inject(FormBuilder);
+  authService = inject(AuthService);
+
+  form = this.fb.nonNullable.group({
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+
+  errorMessage: string | null = null;
+
+  onSubmit(): void {
+    const rawForm = this.form.getRawValue()
+    this.authService.login(rawForm.email, rawForm.password).subscribe({
+      next: () => {
+      this.router.navigate(['/']);
+    },
+    error: (err) => {
+      this.errorMessage = err.code;
+    }
+  })
+
+  }
+
 
 }
--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'amader-chuti-signup',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
 export class SignupComponent {
+
+  router = inject(Router);
+  fb = inject(FormBuilder);
+  authService = inject(AuthService);
+
+  form = this.fb.nonNullable.group({
+    username: ['', Validators.required],
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+
+  errorMessage: string | null = null;
+
+  onSubmit(): void {
+    const rawForm = this.form.getRawValue()
+    this.authService.register(rawForm.email, rawForm.username, rawForm.password).subscribe({
+      next: () => {
+      this.router.navigate(['/login']);
+    },
+    error: (err) => {
+      this.errorMessage = err.code;
+    }
+  })
+
+  }
 
 }
