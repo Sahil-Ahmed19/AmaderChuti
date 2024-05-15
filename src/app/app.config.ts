aac1b12e913d81d8a5070105f4f1a93c2b2cac7d
Sahil Ahmed <74287874+Sahil-Ahmed19@users.noreply.github.com>
2024-05-15 17:32:21 +0530

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,13 +8,7 @@ import { initializeApp } from 'firebase/app';
 import {getAuth, provideAuth} from '@angular/fire/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAqyRN4tYZyVSGCgY-yrpBB6wwYel3LMUs",
-  authDomain: "amader-chuti.firebaseapp.com",
-  projectId: "amader-chuti",
-  storageBucket: "amader-chuti.appspot.com",
-  messagingSenderId: "840569320824",
-  appId: "1:840569320824:web:38f2436ddcbd0e3ef5848d",
-  measurementId: "G-VLQ6PP3P7D"
+  
 };
 
 export const appConfig: ApplicationConfig = {
