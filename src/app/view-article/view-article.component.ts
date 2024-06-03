dad6bf5cb29989fd53b2942185467375b665e2e7
SHYAMSTEEL\sahil.ahmed <sahil.ahmed@shyamsteel.com>
2024-06-03 16:42:46 +0530

    checkbox binding done

--- src/app/view-article/view-article.component.ts
+++ src/app/view-article/view-article.component.ts
@@ -1,16 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { NgxSpinnerModule, NgxSpinnerService } from 'ngx-spinner';
 import { MaterialModule } from '../material/material.module';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'amader-chuti-view-article',
   standalone: true,
-  imports: [NgxSpinnerModule, MaterialModule],
+  imports: [NgxSpinnerModule, MaterialModule, ReactiveFormsModule ],
   templateUrl: './view-article.component.html',
   styleUrl: './view-article.component.scss'
 })
 export class ViewArticleComponent implements OnInit {
+
+  @ViewChild('vc_editors_choice') vc_editors_choice!: ElementRef;
+  vld_editors_choice = new FormControl()
 
   data: any;
 
@@ -25,9 +29,14 @@ export class ViewArticleComponent implements OnInit {
     this.route.queryParams.subscribe(params => {
       if (params) {
         this.data = JSON.parse(params['data']);
+        console.log(this.data);
       }
       this.spinner.hide();
     });
+  }
+
+  submit(value: number){
+    console.log(this.vld_editors_choice.value);
   }
 
 }
